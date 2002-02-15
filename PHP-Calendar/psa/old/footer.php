eee6040d24e84ee7dd22e10bf4c3cdfc11d6c7e4
Sean Proctor <sproctor@gmail.com>
2002-02-15 21:43:48 +0000

    *** empty log message ***

--- PHP-Calendar/psa/old/footer.php
+++
@@ -1,6 +0,0 @@
-<p><a class="plain" href="http://validator.w3.org/check/referer">
-<img src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01!"></a>
-<a class="plain" href="http://jigsaw.w3.org/css-validator/check/referer">
-<img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!"></a></p>
-</body>
-</html>
