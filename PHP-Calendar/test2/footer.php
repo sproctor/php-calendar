3cb7632ad8807d06b22fc7fa7faec1ab41e998f8
Sean Proctor <sproctor@gmail.com>
2002-02-15 21:53:18 +0000

    *** empty log message ***

--- PHP-Calendar/test2/footer.php
+++
@@ -1,6 +0,0 @@
-<p><a class="plain" href="http://validator.w3.org/check/referer">
-<img src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01!"<? if(isold()) echo " border=0"; ?>></a>
-<a class="plain" href="http://jigsaw.w3.org/css-validator/check/referer">
-<img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!"<? if(isold()) echo " border=0"; ?>></a></p>
-</body>
-</html>
