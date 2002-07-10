2809d81fdc207c71b2da74b6b11f535b51759d69
Sean Proctor <sproctor@gmail.com>
2002-07-10 21:47:49 +0000

    I forgot to commit these

---
+++ php-calendar/eventsub.php
@@ -0,0 +1,34 @@
+<?php
+/*
+    Copyright 2002 Sean Proctor
+
+    This file is part of PHP-Calendar.
+
+    PHP-Calendar is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    PHP-Calendar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PHP-Calendar; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+include_once("calendar.inc");
+include_once("config.inc");
+include_once("eventsub.inc");
+
+echo top();
+
+echo submit_event();
+
+echo back_to_calendar();
+
+echo bottom();
+
+?>
