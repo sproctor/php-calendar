90a105a530e47c346cdb221704aa350c81fb90e0
Sean Proctor <sproctor@gmail.com>
2012-07-23 18:24:47 +0000

    Show settings page after settings are submitted

--- php-calendar/includes/settings.php
+++ php-calendar/includes/settings.php
@@ -1,6 +1,6 @@
 <?php 
 /*
- * Copyright 2010 Sean Proctor
+ * Copyright 2012 Sean Proctor
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,18 +21,18 @@ if ( !defined('IN_PHPC') ) {
 
 function settings()
 {
-	global $phpcdb, $phpc_user;
+	global $vars, $phpcdb, $phpc_user;
 
-	if(is_user()) {
-		$forms = array();
+	if(!empty($vars["phpc_submit"]))
+		settings_submit();
 
-		if($phpc_user->is_password_editable())
-			$forms[] = password_form();
-		$forms[] = config_form();
-		return $forms;
-	} else {
-		return config_form();
-	}
+	$forms = array();
+
+	if(is_user() && $phpc_user->is_password_editable())
+		$forms[] = password_form();
+
+	$forms[] = config_form();
+	return $forms;
 }
 
 function password_form()
@@ -84,12 +84,13 @@ function config_form()
 	return tag('form', attributes("action=\"$phpc_script\"",
 				'method="post"'),
 			tag('table', attributes("class=\"phpc-container\""),
-				tag('caption', _('Timezone')),
+				tag('caption', _('Settings')),
 				tag('tfoot',
 					tag('tr',
 						tag('td', attributes('colspan="2"'),
 							create_hidden('phpc_token', $_SESSION['phpc_token']),
-							create_hidden('action', 'settings_submit'),
+							create_hidden('action', 'settings'),
+							create_hidden('phpc_submit', 'settings'),
 							create_submit(_('Submit'))))),
 				tag('tbody',
 					tag('tr',
@@ -99,6 +100,32 @@ function config_form()
 						tag('th', _('Language')),
 						tag('td', $lang_input))
 				   )));
+}
+
+function settings_submit()
+{
+	global $phpcid, $vars, $phpcdb, $phpc_user_tz, $phpc_user_lang;
+
+	verify_token();
+
+        if(empty($vars['timezone']) || empty($vars['language'])) {
+                return tag('div', _('Form error.'));
+	}
+
+	// Expire 20 years in the future, give or take.
+	$expiration_time = time() + 20 * 365 * 24 * 60 * 60;
+	setcookie("phpc_tz", $vars['timezone'], $expiration_time);
+	setcookie("phpc_lang", $vars['language'], $expiration_time);
+
+	if(is_user()) {
+		$uid = $_SESSION['phpc_uid'];
+		$phpcdb->set_timezone($uid, $vars['timezone']);
+		$phpcdb->set_language($uid, $vars['language']);
+		$phpc_user_tz = $vars["timezone"];
+		$phpc_user_lang = $vars["language"];
+	}
+
+        return message(_('Settings updated.'));
 }
 
 ?>
