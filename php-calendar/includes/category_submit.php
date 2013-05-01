4f4800a92f2545d10fd9a6cd07d081d49ae4640c
Sean Proctor <sproctor@gmail.com>
2013-05-01 11:57:29 -0700

    Make category editing more robust.

--- php-calendar/includes/category_submit.php
+++ php-calendar/includes/category_submit.php
@@ -38,6 +38,10 @@ function category_submit()
 	//   values with the "#"
 	$text_color = '#'.$vars["text-color"];
 	$bg_color = '#'.$vars["bg-color"];
+	if(empty($vars['gid']) || strlen($vars['gid']) == 0)
+		$gid = 0;
+	else
+		$gid = $vars['gid'];
 
 	if(!check_color($text_color) || !check_color($bg_color))
 		soft_error(__("Invalid color."));
@@ -56,7 +60,7 @@ function category_submit()
 				permission_error(__('You do not have permission to add categories to this calendar.'));
 		}
 		$catid = $phpcdb->create_category($cid, $vars["name"],
-				$text_color, $bg_color, $vars['gid']);
+				$text_color, $bg_color, $gid);
 	} else {
 		$modify = true;
 
@@ -69,7 +73,7 @@ function category_submit()
 			soft_error(__("You do not have permission to modify this category."));
 			
 		$phpcdb->modify_category($catid, $vars['name'],
-				$text_color, $bg_color, $vars['gid']);
+				$text_color, $bg_color, $gid);
 	}
 
 	$page = "$phpc_script?action=cadmin";
