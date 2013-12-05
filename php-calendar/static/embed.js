d86ccb05663f57b8414badbb8adc32c666f01116
Sean Proctor <sproctor@gmail.com>
2013-12-05 07:19:53 -0800

    add forgotten embed.js

---
+++ php-calendar/static/embed.js
@@ -0,0 +1,21 @@
+function _resize() {
+    var height = $(document.documentElement).height();
+    console.log("height: " + height);
+    // Backwards . send message to parent
+    window.parent.postMessage(['setHeight', height], '*');
+}
+
+console.log("loading page: " + document.URL);
+var resize = _.debounce(_resize, 50);
+
+$(document).ready(function() {
+  resize();
+  $('a').each(function() {
+    if (this.href.indexOf("?") !== -1) {
+      this.href += "&content=embed";
+    } else {
+      this.href += "?content=embed";
+    }
+  });
+});
+
