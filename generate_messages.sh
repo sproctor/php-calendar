1993a4809545cc4d522d3083c8a814d2ae77d4bb
Sean Proctor <sproctor@gmail.com>
2022-01-28 09:20:58 -0500

    add messages.po source, update generation tool

--- generate_messages.sh
+++ generate_messages.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
-xgettext --language=PHP --keyword="__" --keyword="__p:1c,2" --copyright-holder="Sean Proctor" --package-name="PHP-Calendar" \
-	--package-version="2.0.2" --msgid-bugs-address="sproctor@gmail.com" *.php includes/*.php
+xgettext --language=PHP --keyword="__" --keyword="__p:1c,2" --copyright-holder="Sean Proctor" \
+	--package-name="PHP-Calendar" \
+	--package-version="2.0.13" --msgid-bugs-address="sproctor@gmail.com" *.php src/*.php
 sed '0,/SOME DESCRIPTIVE TITLE/s//PHP-Calendar/' messages.po > messages.tmp
 mv messages.tmp messages.po
-sed '0,/YEAR/s//2014/' messages.po > messages.tmp
+sed '0,/YEAR/s//2022/' messages.po > messages.tmp
 mv messages.tmp messages.po
 sed '0,/PACKAGE/s//PHP-Calendar/' messages.po > messages.tmp
 mv messages.tmp messages.po
