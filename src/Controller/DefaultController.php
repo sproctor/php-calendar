fa6db3ea7df026d6b1af2a8f2443f8b9150f58f2
Sean Proctor <sproctor@gmail.com>
2023-07-19 16:01:00 -0400

    re-add DefaultController

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * Copyright Sean Proctor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace App\Controller;
+
+use App\Repository\CalendarRepository;
+use Doctrine\DBAL\Exception\ConnectionException;
+use Doctrine\DBAL\Exception\TableNotFoundException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Controller used to redirect to the month view.
+ *
+ * @author Sean Proctor <sproctor@gmail.com>
+ */
+class DefaultController extends AbstractController
+{
+
+    #[Route("/", name: "default")]
+    public function index(CalendarRepository $repository): Response
+    {
+        $template_variables = [];
+        try {
+            $calendars = $repository->findAll();
+        } catch (ConnectionException | TableNotFoundException) {
+            return $this->redirectToRoute('setup');
+        }
+
+        $template_variables["calendars"] = $calendars;
+
+        if (empty($calendars)) {
+            return $this->redirectToRoute('setup');
+        } else {
+            return $this->render("calendar_list.html.twig", $template_variables);
+        }
+    }
+}
